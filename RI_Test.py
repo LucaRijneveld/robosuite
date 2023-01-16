ce0947dd4c7881081a085f0d5fde76981090f84e
LucaRijneveld <lucarijneveld@hotmail.com>
2023-01-16 19:37:36 +0100

    Added customgym env to the RI_Test script

--- RI_Test.py
+++ RI_Test.py
@@ -3,14 +3,11 @@ from turtle import delay
 import robosuite as suite
 import numpy as np
 import time
+from CustomGym import RoboEnv
 
 model = PPO.load("/home/howl/robosuite/robosuite-1/model.zip", print_system_info=True)
 
-env = suite.make(env_name="PickPlace",
-                 robots="Panda",
-                 has_renderer=True,
-                 has_offscreen_renderer=False,
-                 use_camera_obs = False)
+env = RoboEnv(RenderMode = False)
 
 obs = env.reset()
 for i in range(1000):
